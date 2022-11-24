54993e1ac3e1259eef3dad0fafe9067e53b95f8d
Nicholas <nicholasgalante94@gmail.com>
2022-11-24 16:27:41 -0500

    commit tree fe61d584a0e85e8be60d918760b546797d4b6a5b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+findMatching = (drivers, string) => {
+    func = drivers => drivers.toUpperCase() === string.toUpperCase();
+    return drivers.filter(func);
+}
+
+fuzzyMatch = (drivers, string) => {
+    func = drivers => drivers[0] === string[0];
+    return drivers.filter(func);
+}
+
+matchName = (drivers, string) => {
+    func = drivers => drivers.name === string;
+    return drivers.filter(func);
+}
